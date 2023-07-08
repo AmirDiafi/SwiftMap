1cd35fd3eb555e56f03f1eb8ee5c4a718216b315
Amir Diafi <59441376+AmirDiafi@users.noreply.github.com>
2023-07-08 21:43:29 +0100

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Amir Diafi on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
